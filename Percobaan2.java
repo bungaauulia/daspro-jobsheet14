0686221ffbb8eb7948513761cb90ed511e8333f2
bungaauulia <bungaaulia1215@gmail.com>
2024-12-01 21:41:36 +0700

    commit mencetak deret hitung pangkat

--- Percobaan2.java
+++ Percobaan2.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
 public class Percobaan2 {
-    static int hitungPangkat (int x, int y) {
+    static StringBuilder deret = new StringBuilder();
+
+    static int hitungPangkat(int x, int y) {
         if (y == 0) {
-            return(1);
+            deret.append("1");
+            return 1;
         } else {
-            return ( x * hitungPangkat(x, y - 1));
+            deret.append(x).append("x");
+            return x * hitungPangkat(x, y - 1);
         }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int bilangan, pangkat;
+
         System.out.print("Bilangan yang dihitung: ");
         bilangan = sc.nextInt();
         System.out.print("Pangkat: ");
         pangkat = sc.nextInt();
 
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        int hasil = hitungPangkat(bilangan, pangkat);
+
+        if (deret.charAt(deret.length() - 1) == 'x') {
+            deret.deleteCharAt(deret.length() - 1);
+        }
+        System.out.println(deret + " = " + hasil);
     }
 }
